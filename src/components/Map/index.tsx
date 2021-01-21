88d3cf5011932e97192c441dfa231ae27a7a0dc5
FaizaneMuhammad <faizanemuhammad@A006-00060.local>
2021-01-21 18:58:30 +0500

    Get user location and mark the location on the map using a marker.

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,24 +1,44 @@
 import React, { useRef, useState, useCallback } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
+import { Button } from "react-bootstrap";
 import { MapContainerWrapper } from "components/Map/Map.styles";
+import image from "media/images/pin.png";
 
 const Map = (props: any) => {
   const mapRef = useRef<null | any>(null);
 
   // Component States
+  const [userLocation, setUserLocation]: any | null = useState(null);
   const [viewport, setViewport] = useState({
     width: "100%",
     height: "100%",
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
+    latitude: 4.214,
+    longitude: -138.49,
+    zoom: 0,
   });
 
+  const setUserLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let userLocation = {
+        lat: position.coords.latitude,
+        long: position.coords.longitude,
+      };
+      let newViewport = {
+        height: "100%",
+        width: "100%",
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        zoom: 10,
+      };
+      setViewport(newViewport);
+      setUserLocation(userLocation);
+    });
+  };
+
   // Map function handlers
-  const handleViewportChange = useCallback(
-    (newViewport) => setViewport(newViewport),
-    []
-  );
+  const handleViewportChange = useCallback((newViewport) => {
+    setViewport(newViewport);
+  }, []);
 
   return (
     <div>
@@ -29,7 +49,19 @@ const Map = (props: any) => {
           onViewportChange={handleViewportChange}
           mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
           mapStyle="mapbox://styles/mapbox/basic-v9"
-        />
+        >
+          <Button onClick={setUserLocationHandler}>My Location</Button>
+          {userLocation !== null ? (
+            <Marker
+              latitude={userLocation.lat}
+              longitude={userLocation.long}
+              offsetLeft={-15}
+              offsetTop={-30}
+            >
+              <img src={image} alt="marker" />
+            </Marker>
+          ) : null}
+        </ReactMapGL>
       </MapContainerWrapper>
     </div>
   );

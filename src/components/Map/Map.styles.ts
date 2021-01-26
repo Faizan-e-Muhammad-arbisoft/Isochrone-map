fb1c042d7637c8e4a7ff2d7996d135db0d0bb183
FaizaneMuhammad <faizanemuhammad@A006-00060.local>
2021-01-26 14:30:29 +0500

    Some changes to user interface.

--- src/components/Map/Map.styles.ts
+++ src/components/Map/Map.styles.ts
@@ -4,3 +4,10 @@ export const MapContainerWrapper = styled.div`
   width: 100%;
   height: 700px;
 `;
+
+export const FormWrapper = styled.div`
+  background-color: rgba(255, 255, 255, 0.7);
+  width: 25%;
+  padding: 10px;
+  border-radius: 0 0 25px 0;
+`;
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,8 +1,8 @@
 import React, { useRef, useState, useCallback } from 'react';
 import ReactMapGL, { Marker, FlyToInterpolator, Source, Layer } from 'react-map-gl';
 import { isoLayer } from 'components/Map/layers';
-import { Button, Form, Col } from 'react-bootstrap';
-import { MapContainerWrapper } from 'components/Map/Map.styles';
+import { Button, Form } from 'react-bootstrap';
+import { MapContainerWrapper, FormWrapper } from 'components/Map/Map.styles';
 import image from 'media/images/pin.png';
 
 const Map = (props: any) => {
@@ -68,52 +68,52 @@ const Map = (props: any) => {
           mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
           mapStyle="mapbox://styles/mapbox/basic-v9"
         >
-          {/* userlocation button */}
-          <Button onClick={setUserLocationHandler}>My Location</Button>
+          <FormWrapper>
+            {/* userlocation button */}
+            <Button onClick={setUserLocationHandler}>My Location</Button>
 
-          {/* Isochrone api form */}
-          <Form onSubmit={(e) => fetchDataClickHandler(e)}>
-            <Form.Group>
-              <Form.Label>Duration in minutes</Form.Label>
-              <Col xs={1}>
-                <Form.Control type="number" defaultValue={10} ref={inputRef} />
-              </Col>
-            </Form.Group>
-            <Form.Group controlId="travelModeRadio">
-              <Form.Check
-                inline
-                type="radio"
-                label="Walking"
-                id="walkingRadio"
-                value="walking"
-                name="radio"
-                onChange={travelModeChangeHandler}
-              />
-              <Form.Check
-                inline
-                type="radio"
-                label="Cycling"
-                id="cyclingRadio"
-                value="cycling"
-                name="radio"
-                onChange={travelModeChangeHandler}
-              />
-              <Form.Check
-                inline
-                type="radio"
-                label="Driving"
-                id="drivingRadio"
-                value="driving"
-                name="radio"
-                onChange={travelModeChangeHandler}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Button type="submit" disabled={userLocation === null || travelMode === null}>
-                Isochrone Api
-              </Button>
-            </Form.Group>
-          </Form>
+            {/* Isochrone api form */}
+            <Form onSubmit={(e) => fetchDataClickHandler(e)}>
+              <Form.Group>
+                <Form.Label>Duration in minutes</Form.Label>
+                <Form.Control type="number" defaultValue={10} min={1} ref={inputRef} />
+              </Form.Group>
+              <Form.Group controlId="travelModeRadio">
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="Walking"
+                  id="walkingRadio"
+                  value="walking"
+                  name="radio"
+                  onChange={travelModeChangeHandler}
+                />
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="Cycling"
+                  id="cyclingRadio"
+                  value="cycling"
+                  name="radio"
+                  onChange={travelModeChangeHandler}
+                />
+                <Form.Check
+                  inline
+                  type="radio"
+                  label="Driving"
+                  id="drivingRadio"
+                  value="driving"
+                  name="radio"
+                  onChange={travelModeChangeHandler}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Button type="submit" disabled={userLocation === null || travelMode === null}>
+                  Isochrone Api
+                </Button>
+              </Form.Group>
+            </Form>
+          </FormWrapper>
 
           {/* user location marker */}
           {userLocation !== null ? (
